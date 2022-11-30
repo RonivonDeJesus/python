e989bc750b411b51eac91561b2e2c10c05c63b77
Ronivon de Jesus Andrade <ronivon.andrade99@gmail.com>
2022-11-30 09:53:42 -0300

    adicionando master

---
+++ receitas/urls.py
@@ -0,0 +1,13 @@
+from django.urls import include, path
+from django.contrib import admin
+from django.conf.urls.static import static
+from django.conf import settings
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('<int:receita_id>', views.receita, name='receita'),
+    path('busca', views.buscar, name='buscar'),
+    path('admin/', admin.site.urls),
+    path('usuarios/', include('usuarios.urls'))
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
