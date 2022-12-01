d9fcd117ce355c9c716830d0fc4f8581960d5073
Ronivon de Jesus Andrade <ronivon.andrade99@gmail.com>
2022-12-01 13:05:25 -0300

    refatorando templates

--- usuarios/views.py
+++ usuarios/views.py
@@ -104,7 +104,7 @@ def cria_receita(request):
         messages.success(request, 'Receita cadastrada com sucesso')
         return redirect('dashboard')
     else:
-        return render(request, 'usuarios/cria_receita.html')
+        return render(request, 'receitas/cria_receita.html')
 
 def deleta_receita(request, receita_id):
     receita = get_object_or_404(Receita, pk=receita_id)
@@ -116,7 +116,7 @@ def edita_receita(request, receita_id):
     receita_a_editar = {
         'receita': receita
     }
-    return render(request, 'usuarios/edita_recita.html', receita_a_editar)
+    return render(request, 'receitas/edita_recita.html', receita_a_editar)
 
 def atualiza_receita(request):
     if request.method == 'POST':
