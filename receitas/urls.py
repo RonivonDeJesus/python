9b632563021496f2a194845b7bdef667af0fa70d
Ronivon de Jesus Andrade <ronivon.andrade99@gmail.com>
2022-12-01 12:55:38 -0300

    retirando o teste

--- receitas/urls.py
+++ receitas/urls.py
@@ -9,6 +9,5 @@ urlpatterns = [
     path('<int:receita_id>', views.receita, name='receita'),
     path('busca', views.buscar, name='buscar'),
     path('admin/', admin.site.urls),
-    path('usuarios/', include('usuarios.urls')),
-    
+    path('usuarios/', include('usuarios.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
